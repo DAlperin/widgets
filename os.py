aa37028bd0e321fcca6a078f7eac14fc9fa82b51
Beam Center <beamcenter@BrianCocbookPro.home>
2017-05-16 16:35:48 -0400

    hi

--- os.py
+++ os.py
@@ -14,7 +14,7 @@ import time
 from papirus import PapirusText
 from papirus import PapirusImage
 imagefun = PapirusImage()
-GPIO.setmode(GPIO.BOARD)
+GPIO.set mode(GPIO.BOARD)
 
 text = PapirusText()
 
@@ -28,28 +28,7 @@ BLACK = 0
 
 while True:
     if GPIO.input(button1) == False:
-        ia=$(echo "ibase=16;$(i2cget -y 1 0x48 0x00 w | sed -e 's/^0x//g' | sed -n 's/\([0-9a-f][0-9a-f]\)\([0-9a-f][0-9a-f]\)/\2\1/p'| tr [[:lower:]] [[:upper:]])" | bc)
-        [ $DEBUG ] && echo "IA is ${ia}"
-        #    convert that to binary
-        ib=$(echo "obase=2;$ia" | bc)
-        [ $DEBUG ] && echo "IB is ${ib}"
-        # strip last 5 bits
-        ic="${ib:0:-5}"
-        [$DEBUG ] && echo "IC is ${ic}"
-        # if binary value is 11 bits long, number is negative (twos complement)
-        if [ $(echo -en "${ic}" | wc -c) -eq 11 ]
-            then
-            [ $DEBUG ] && echo "Temperature is negative"
-            ica=$(echo "ibase=2;${ic}" | bc)
-            id=$(echo "-2048+${ica}" | bc)
-        else
-            id=$(echo "ibase=2;${ic}" | bc)
-            fi
-            [ $DEBUG ] && echo "ID is ${id}"
-            ie=$(echo "scale=3;${id}*0.125" | bc)
-        if=$(echo "scale=3;${ie}*1.8+32" | bc)
-            echo "Temp is ${ie} deg C (${if} deg F)."
-            papirus-write "Temp is ${ie} deg C (${if} deg F)."
+        text.write("hi")
     if GPIO.input(button2) == False:
         print("button2 pressed")
         text.write("PaPiRus Buttons  Button 2 pressed")
