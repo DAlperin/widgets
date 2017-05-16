96870cd7b0a8dbe98af3260037a8d52b7fe56e29
Beam Center <beamcenter@BrianCocbookPro.home>
2017-05-16 16:49:50 -0400

    adsf

--- os.py
+++ os.py
@@ -1,44 +1,144 @@
+#!/usr/bin/env python
 
-#!/usr/bin/python
-#frownt = front
 import os
 import sys
-
+import string
+from papirus import Papirus
 from PIL import Image
 from PIL import ImageDraw
+from PIL import ImageFont
+from time import sleep
 import RPi.GPIO as GPIO
-from papirus import Papirus
-import random
-import time
 
-from papirus import PapirusText
-from papirus import PapirusImage
-imagefun = PapirusImage()
-GPIO.set mode(GPIO.BOARD)
+# Check EPD_SIZE is defined
+EPD_SIZE=0.0
+if os.path.exists('/etc/default/epd-fuse'):
+    execfile('/etc/default/epd-fuse')
+if EPD_SIZE == 0.0:
+    print("Please select your screen size by running 'papirus-config'.")
+    sys.exit()
 
-text = PapirusText()
+# Running as root only needed for older Raspbians without /dev/gpiomem
+if not (os.path.exists('/dev/gpiomem') and os.access('/dev/gpiomem', os.R_OK | os.W_OK)):
+    user = os.getuid()
+    if user != 0:
+        print("Please run script as root")
+        sys.exit()
 
-# Write text to the screen
+# Command line usage
+# papirus-buttons
+
+hatdir = '/proc/device-tree/hat'
+
 WHITE = 1
 BLACK = 0
 
+SIZE = 27
 
+# Assume Papirus Zero
+SW1 = 21
+SW2 = 16
+SW3 = 20 
+SW4 = 19
+SW5 = 26
 
+# Check for HAT, and if detected redefine SW1 .. SW5
+if (os.path.exists(hatdir + '/product')) and (os.path.exists(hatdir + '/vendor')) :
+   f = open(hatdir + '/product')
+   prod = f.read()
+   f.close()
+   f = open(hatdir + '/vendor')
+   vend = f.read()
+   f.close
+   if (string.find(prod, 'PaPiRus ePaper HAT') == 0) and (string.find(vend, 'Pi Supply') == 0) :
+       # Papirus HAT detected
+       SW1 = 16
+       SW2 = 26
+       SW3 = 20
+       SW4 = 21
+       SW5 = -1
 
+def main():
+    global SIZE
 
-while True:
-    if GPIO.input(button1) == False:
-        print("button2 pressed")
-        text.write("PaPiRus Buttons  Button 2 pressed")
-    if GPIO.input(button2) == False:
-        print("button2 pressed")
-        text.write("PaPiRus Buttons  Button 2 pressed")
-    if GPIO.input(button3) == False:
-        print("button3 pressed")
-        text.write("PaPiRus Buttons  Button 3 pressed")
-    if GPIO.input(button4) == False:
-        print("button4 pressed")
-        text.write("PaPiRus Buttons  Button 4 pressed")    
-    if GPIO.input(button5) == False:
-        print("button5 pressed")
-        text.write("PaPiRus Buttons  Button 5 pressed")  
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(SW1, GPIO.IN)
+    GPIO.setup(SW2, GPIO.IN)
+    GPIO.setup(SW3, GPIO.IN)
+    GPIO.setup(SW4, GPIO.IN)
+    if SW5 != -1:
+        GPIO.setup(SW5, GPIO.IN)
+
+    papirus = Papirus()
+
+    # Use smaller font for smaller dislays
+    if papirus.height <= 96:
+        SIZE = 18
+
+    papirus.clear()
+
+    write_text(papirus, "Ready... SW1 + SW2 to exit.", SIZE)
+
+    while True:
+        # Exit when SW1 and SW2 are pressed simultaneously
+        if (GPIO.input(SW1) == False) and (GPIO.input(SW2) == False) :
+            write_text(papirus, "Exiting ...", SIZE)
+            sleep(0.2)
+            papirus.clear()
+            sys.exit()
+
+        if GPIO.input(SW1) == False:
+            write_text(papirus, "One", SIZE)
+
+        if GPIO.input(SW2) == False:
+            write_text(papirus, "Two", SIZE)
+
+        if GPIO.input(SW3) == False:
+            write_text(papirus, "Three", SIZE)
+
+        if GPIO.input(SW4) == False:
+            write_text(papirus, "Four", SIZE)
+
+        if (SW5 != -1) and (GPIO.input(SW5) == False):
+            write_text(papirus, "Five", SIZE)
+ 
+        sleep(0.1)
+
+def write_text(papirus, text, size):
+
+    # initially set all white background
+    image = Image.new('1', papirus.size, WHITE)
+
+    # prepare for drawing
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', size)
+
+    # Calculate the max number of char to fit on line
+    line_size = (papirus.width / (size*0.65))
+
+    current_line = 0
+    text_lines = [""]
+
+    # Compute each line
+    for word in text.split():
+        # If there is space on line add the word to it
+        if (len(text_lines[current_line]) + len(word)) < line_size:
+            text_lines[current_line] += " " + word
+        else:
+            # No space left on line so move to next one
+            text_lines.append("")
+            current_line += 1
+            text_lines[current_line] += " " + word
+
+    current_line = 0
+    for l in text_lines:
+        current_line += 1
+        draw.text( (0, ((size*current_line)-size)) , l, font=font, fill=BLACK)
+
+    papirus.display(image)
+    papirus.partial_update()
+
+if __name__ == '__main__':
+    main()
